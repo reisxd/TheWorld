910997e39e5d41d3f994988db6758f9fb242515f
GramingFoxTeam <reis@turkeylist.gq>
2023-01-18 19:40:32 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+Console.WriteLine("Welcome to The World! Please wait.");
+
+Assembly assembly = Assembly.GetExecutingAssembly();
+
+Stream stream = assembly.GetManifestResourceStream("TheWorld.Resources.TOOLWIZTIMEFREEZE.CONFIG");
+stream.Seek(0, SeekOrigin.Begin);
+
+MemoryStream mem = new MemoryStream();
+stream.CopyTo(mem);
+
+if (Directory.Exists("C:\\TOOLWIZTIMEFREEZE"))
+{
+    if (!File.Exists("C:\\TOOLWIZTIMEFREEZE\\TOOLWIZTIMEFREEZE-oldconf.CONFIG")) File.Copy("C:\\TOOLWIZTIMEFREEZE\\TOOLWIZTIMEFREEZE.CONFIG", "C:\\TOOLWIZTIMEFREEZE\\TOOLWIZTIMEFREEZE-oldconf.CONFIG");
+    File.Delete("C:\\TOOLWIZTIMEFREEZE\\TOOLWIZTIMEFREEZE.CONFIG");
+    File.WriteAllBytes("C:\\TOOLWIZTIMEFREEZE\\TOOLWIZTIMEFREEZE.CONFIG", mem.ToArray());
+    Console.WriteLine("Done! The password to unlock is 1234.");
+}
+else
+{
+    Console.WriteLine("Could not find Toolwiz TimeFreeze! Are you sure it's installed?");
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ README.md
@@ -0,0 +1,13 @@
+# The World
+
+The World allows you to change the password of Toolwiz TimeFreeze. 
+
+Note: The World only works with Toolwiz TimeFreeze 2016.
+
+# Usage
+
+You'll need at least .NET 6 to run The World.
+
+Firstly, build the project and run the executable. It'll replace the configuration file and change the password to `1234`.
+
+If it can't access the configuration files, try running the executable as administrator.
